78125405ea20a628823280a9c42c64970e5a1198
qa <qingan0627@gmail.com>
2025-10-26 02:58:00 +0800

    出錯了要重來嗚嗚

--- apps/food/apps.py
+++ apps/food/apps.py
@@ -3,4 +3,6 @@ from django.apps import AppConfig
 
 class FoodConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'food'
+    # The name must be the full Python path to the app package so Django
+    # can import it correctly when INSTALLED_APPS contains 'apps.food'.
+    name = 'apps.food'
